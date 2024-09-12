6b21b9422b41de47833587fba31aaf4e45a96032
Ramplastinskij <roman.zhilkin.21@yandex.ru>
2024-09-12 21:08:41 +0500

    Tasks solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+            Author author1 = new Author("Leo", "Tolstoy");
+            Author author2 = new Author("Fiodor", "Dostoevsky");
+
+            Book book1 = new Book("War and Peace", author1, 1869);
+            Book book2 = new Book("Crime and Punishment", author2, 1866);
+
+            book1.setPublicationYear(1873);
+
+            System.out.println(book1.getTitle() + " by " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() +
+                    ", published in "+ book1.getPublicationYear());
+
+            System.out.println(book2.getTitle() + " by " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName() +
+                    ", published in " + book2.getPublicationYear());
+        }
     }
-}
